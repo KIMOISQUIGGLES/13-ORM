caba5094ed48aebc57b501c3f1d59685cadd4263
Tyler Alcover <alcovertyler@gmail.com>
2022-06-15 03:22:47 -0700

    finishing

---
+++ Develop/routes/api/tag-routes.js
@@ -0,0 +1,90 @@
+const router = require('express').Router();
+const { Tag, Product, ProductTag } = require('../../models');
+
+// The `/api/tags` endpoint
+
+router.get('/', (req, res) => {
+  // find all tags
+  // be sure to include its associated Product data
+  Tag.findAll({
+    include: [Product]
+  })
+  .then(dbProducts => {
+    res.json(dbProducts);
+  })
+  .catch(err => {
+    console.log(err);
+    res.status(500).json({
+      msg: "an error occured",
+      err
+    });
+  });
+});
+
+router.get('/:id', (req, res) => {
+  // find a single tag by its `id`
+  // be sure to include its associated Product data
+  Tag.findByPk(req.params.id, {
+    include: [Product]
+  })
+  .then(dbProducts => {
+    res.json(dbProducts);
+  })
+  .catch(err => {
+    console.log(err);
+    res.status(500).json({
+      msg: "an error occured",
+      err
+    });
+  });
+});
+
+router.post('/', (req, res) => {
+  // create a new tag
+  Tag.create(req.body)
+    .then(newCategory => {
+      res.json(newCategory);
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json({
+        msg: "an error occured",
+        err
+      });
+    });
+});
+
+router.put('/:id', (req, res) => {
+  // update a tag's name by its `id` value
+  Tag.update(req.body, {
+    where : {
+      id: req.params.id
+    }
+  }) .then(updatedTag => {
+    res.json(updatedTag);
+  })
+  .catch (err => {
+    console.log(err);
+    res.status(500).json({msg: "Error occurred.", err})
+  })
+});
+
+router.delete('/:id', (req, res) => {
+  // delete on tag by its `id` value
+  Tag.destroy({
+    where: {
+      id: req.params.id
+    }
+  }).then(delTag => {
+    res.json(delTag);
+  })
+  .catch(err => {
+    console.log(err);
+    res.status(500).json({
+      msg: "an error occured",
+      err
+    });
+  });
+});
+
+module.exports = router;
